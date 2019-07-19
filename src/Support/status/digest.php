84dea8d91c9e43e5d430b679b15c09014c3d3418
Deniz Tezcan <howdy@deniztezcan.me>
2019-07-19 11:26:15 +0200

    Small fuck-up when coming to generating the digest for a AcquirerStatusReq...

--- src/Support/status/digest.php
+++ src/Support/status/digest.php
@@ -9,7 +9,7 @@
  */
 
 return [
-    'AcquirerTrxReq' => [
+    'AcquirerStatusReq' => [
         '_attributes' => [
             'xmlns' => 'http://www.idealdesk.com/ideal/messages/mer-acq/3.3.1',
             'version' => '3.3.1',
