fc34a5b5c967b14b82389fdbb732bf9a52e1ad62
otrishyn <alexey_trishin@bintime.com>
2020-01-15 17:01:59 +0200

    fix undefined index

--- src/Message/Response/CompletePurchase.php
+++ src/Message/Response/CompletePurchase.php
@@ -23,62 +23,62 @@ class CompletePurchase extends AbstractResponse
 
     public function getTransaction()
     {
-        return $this->data['Transaction'];
+        return $this->data['Transaction'] ?? [];
     }
 
     public function getTransactionID()
     {
         if (isset($this->data['Transaction'])) {
-            return (string) $this->data['Transaction']['transactionID'];
+            return (string) ($this->data['Transaction']['transactionID'] ?? '');
         }
     }
 
     public function getStatus()
     {
         if (isset($this->data['Transaction'])) {
-            return (string) $this->data['Transaction']['status'];
+            return (string) ($this->data['Transaction']['status'] ?? '');
         }
     }
 
     public function getStatusDateTimestamp()
     {
         if (isset($this->data['Transaction'])) {
-            return (string) $this->data['Transaction']['statusDateTimestamp'];
+            return (string) ($this->data['Transaction']['statusDateTimestamp'] ?? '');
         }
     }
 
     public function getConsumerName()
     {
         if (isset($this->data['Transaction'])) {
-            return (string) $this->data['Transaction']['consumerName'];
+            return (string) ($this->data['Transaction']['consumerName'] ?? '');
         }
     }
 
     public function getConsumerIBAN()
     {
         if (isset($this->data['Transaction'])) {
-            return (string) $this->data['Transaction']['consumerIBAN'];
+            return (string) ($this->data['Transaction']['consumerIBAN'] ?? '');
         }
     }
 
     public function getConsumerBIC()
     {
         if (isset($this->data['Transaction'])) {
-            return (string) $this->data['Transaction']['consumerBIC'];
+            return (string) ($this->data['Transaction']['consumerBIC']  ?? '');
         }
     }
 
     public function getAmount()
     {
         if (isset($this->data['Transaction'])) {
-            return (float) $this->data['Transaction']['amount'];
+            return (float) ($this->data['Transaction']['amount'] ?? 0.00);
         }
     }
 
     public function getCurrency()
     {
         if (isset($this->data['Transaction'])) {
-            return (string) $this->data['Transaction']['currency'];
+            return (string) ($this->data['Transaction']['currency'] ?? '');
         }
     }
 }
